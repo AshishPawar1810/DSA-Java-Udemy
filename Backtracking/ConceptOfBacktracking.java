612de65b21c96b76959625e102530d25674cec72
AshishPawar1810 <80397555+AshishPawar1810@users.noreply.github.com>
2025-08-20 10:01:26 +0530

    Backtracking 20 aut 25

---
+++ Backtracking/ConceptOfBacktracking.java
@@ -0,0 +1,14 @@
+package Backtracking;
+/*
+Baktracking :
+to a problem we have number of solution
+but every time it is not possbile all solution were work
+for those we check with method
+if it is not working then we back track it
+and start with new method
+
+ */
+public class ConceptOfBacktracking {
+
+
+}
---
+++ Backtracking/RatInMaze.java
@@ -0,0 +1,16 @@
+package Backtracking;
+/*
+
+ */
+public class RatInMaze {
+
+
+
+    public static void main(String[] args) {
+        int maze[][] = {{1, 0, 0, 0},
+                        {1, 1, 0, 1},
+                        {0, 1, 0, 0},
+                        {1, 1, 1, 1}};
+    }
+}
+
